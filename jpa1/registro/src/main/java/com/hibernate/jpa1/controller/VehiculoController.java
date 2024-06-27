f52756a6397d404aece3de583324f45d85764885
Giani <Giani@DESKTOP-STIGMAN>
2024-06-27 15:47:17 -0300

    Modificacion del controlador de vehiculo, se agregaron los endpoints getVehiculos y postVehiculo

--- jpa1/registro/src/main/java/com/hibernate/jpa1/controller/VehiculoController.java
+++ jpa1/registro/src/main/java/com/hibernate/jpa1/controller/VehiculoController.java
@@ -4,10 +4,33 @@
  */
 package com.hibernate.jpa1.controller;
 
+import com.hibernate.jpa1.model.Vehiculo;
+import com.hibernate.jpa1.service.IVehiculoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Giani
  */
+@RestController
 public class VehiculoController {
     
+    @Autowired
+    private IVehiculoService vehiculoServ;
+    
+    @GetMapping("/vehiculos/traer")
+    public List<Vehiculo> getVehiculos(){
+        return vehiculoServ.getVehiculos();
+    }
+    
+    @PostMapping("/vehiculo/crear")
+    public String saveVehiculo(@RequestBody Vehiculo vehiculo){
+        vehiculoServ.saveVehiculo(vehiculo);
+        return "El vehiculo fue creado correctamente";
+    }
 }
