c52f93b00bdd9ccfc9efee145f46f502a94d2dc1
Giani <Giani@DESKTOP-STIGMAN>
2024-08-15 15:20:18 -0300

    Commit de datos no guardados

--- cursos/src/main/java/com/stig/cursos/model/Tema.java
+++ cursos/src/main/java/com/stig/cursos/model/Tema.java
@@ -4,10 +4,36 @@
  */
 package com.stig.cursos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
 /**
  *
  * @author Giani
  */
+@Getter @Setter
+@Entity
 public class Tema {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private Long idTema;
+    private String nombre;
+    private String descripcion;
+
+    public Tema() {
+    }
+
+    public Tema(Long idTema, String nombre, String descripcion) {
+        this.idTema = idTema;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    
     
 }
